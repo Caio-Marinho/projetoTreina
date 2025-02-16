dab5f030b7c4edbd96b7f1d1e918dd00c811b6b4
Caio-Marinho <kaigabriel12@gmail.com>
2025-02-16 18:34:33 -0300

    Controller e Repository

---
+++ springapi/src/main/java/com/curriculo/springapi/controller/DDDController.java
@@ -0,0 +1,23 @@
+package com.curriculo.springapi.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.curriculo.springapi.model.DDD;
+import com.curriculo.springapi.repository.DDDRepository;
+import com.curriculo.springapi.Service.Service;
+@RestController
+@RequestMapping("/ddd")
+public class DDDController {
+    
+    @PostMapping
+    public ResponseEntity<DDD> createEndereco(@RequestBody DDD ddd) {
+        return Service.cadastrarDDD(ddd,dddRepository);
+    }
+    
+    @Autowired
+    private DDDRepository dddRepository;
+}
